2131e6255bfca6481fc8400cc54dc460fcced91b
taka-2120 <yu_tkhs@icloud.com>
2024-04-24 16:17:37 +0900

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python3
 
+
 # shebang
 class Sudoku:
-	def __init__(self):
-		all_values = input().split(" ")
-		print(problem)
-		self.cells = []
-		for (x, value in enumerate(all_values):
-			// TODO: 
-		self.cells = [[0 for _ in range(9)] for _ in range(9)]
+    def __init__(self):
+        all_values = input().split(" ")
+        # make 9x9 array
+        self.cells = []
+        count = 0
+        for _ in range(9):
+            array = []
+            for i in range(9):
+                array.append(all_values[count])
+                count += 1
+            self.cells.append(array)
 
-	def print_sudoku(self, board):
-    		for i in range(len(board)):
-        		if i % 3 == 0 and i != 0:
-            			print("-" * 23)
-        		for j in range(len(board[0])):
-		            if j % 3 == 0 and j != 0:
-		                print(" |", end="")
-		            if j == 8:
-		                print(" " + str(board[i][j]))
-		            else:
-		                print(" " + str(board[i][j]), end="")
+    def print_sudoku(self, board):
+        for i in range(len(board)):
+            if i % 3 == 0 and i != 0:
+                print("-" * 23)
+            for j in range(len(board[0])):
+                if j % 3 == 0 and j != 0:
+                    print(" |", end="")
+                value = " " if board[i][j] == "-" else board[i][j]
+                if j == 8:
+                    print(" " + value)
+                else:
+                    print(" " + value, end="")
 
-	def dump(self):
-		self.print_sudoku(self.cells)
+    def dump(self):
+        self.print_sudoku(self.cells)
+
 
 def main():
-	sdk = Sudoku()
-	sdk.dump()
+    sdk = Sudoku()
+    sdk.dump()
+
 
 main()
